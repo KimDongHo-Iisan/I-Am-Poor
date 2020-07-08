44a4888220a5c519759a585bb3c5610a2b18491c
KimDongHo-Iisan <68005740+KimDongHo-Iisan@users.noreply.github.com>
2020-07-08 19:44:57 +0900

    Add files via upload

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Dongho kim on 2020/06/25.
+//  Copyright © 2020 Dongho kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
